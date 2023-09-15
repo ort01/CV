d77454a853d5dc7381a7d9de0fb8aed84728fc7b
ort01 <ortutova.kristina@gmail.com>
2023-09-15 10:28:51 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <CV />
+  </div>
+</template>
+
+<script setup>
+import CV from "./components/Cv.vue"
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/Cv.vue
@@ -0,0 +1,486 @@
+<template>
+    <page size="A4">
+        <div class="header__background">
+            <div class="header__content">
+                <div class="header__content--left">
+                    <img src="../../DSC07762-cv.jpg" alt="avatar">
+                </div>
+                <div class="header__content--right">
+                    <h1>Kristína</h1>
+                    <h1>Ortutová</h1>
+                    <h4>Front-end Developer</h4>
+                    <hr>
+                    <div>
+                        <a href="https://github.com/ort01/">
+                            <i class="material-symbols-outlined">
+                                business_center
+                            </i>
+                            <p>My projects</p>
+                        </a>
+                        <a>
+                            <i class="material-symbols-outlined">
+                                link
+                            </i>
+                            <p>This CV is wrote in Vue.js</p>
+
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="body">
+            <div class="body__left">
+                <div class="body__left--contact">
+                    <a href="tel:+421915456007">
+                        <div class="content">
+                            <i class="material-symbols-outlined md-36">
+                                phone_iphone
+                            </i>
+                            <p>+421 915 456 007</p>
+                        </div>
+                    </a>
+                    <a href="mailto:ortutova.kristina@gmail.com">
+                        <div class="content">
+                            <i class="material-symbols-outlined md-36">
+                                mail
+                            </i>
+                            <p>ortutova.kristina@gmail.com</p>
+                        </div>
+                    </a>
+                    <a href="https://www.google.com/maps/place/Komarno/">
+                        <div class="content">
+                            <i class="material-symbols-outlined md-36">
+                                cottage
+                            </i>
+                            <p>Komárno, Slovakia</p>
+                        </div>
+                    </a>
+                </div>
+                <div class="body__left--skills">
+                    <h3>Skills</h3>
+                    <div class="skills-separation">
+                        <div class="content">
+                            <p>Html</p>
+                            <p>Advanced</p>
+                        </div>
+                        <div class="content">
+                            <p>css/scss</p>
+                            <p>Advanced</p>
+                        </div>
+                        <div class="content">
+                        </div>
+                        <div class="content">
+                            <p>Javascript</p>
+                            <p>Intermediate</p>
+                        </div>
+                        <div class="content">
+                            <p>Typescript</p>
+                            <p>Intermediate</p>
+                        </div>
+                        <div class="content">
+                            <p>Vue.js</p>
+                            <p>Advanced</p>
+                        </div>
+                        <div class="content">
+                            <p>React.js</p>
+                            <p>Intermediate</p>
+                        </div>
+                    </div>
+                    <div class="skills-separation">
+                        <div class="content">
+                            <p>Node.js</p>
+                            <p>Beginner</p>
+                        </div>
+                        <div class="content">
+                            <p>Express.js</p>
+                            <p>Beginner</p>
+                        </div>
+                        <div class="content">
+                            <p>Firebase</p>
+                            <p>Beginner</p>
+                        </div>
+                        <div class="content">
+                            <p>MongoDB</p>
+                            <p>Beginner</p>
+                        </div>
+
+                    </div>
+                    <div>
+                        <div class="content">
+                            <p>Docker</p>
+                            <p>Beginner</p>
+                        </div>
+                        <div class="content">
+                            <p>git</p>
+                            <p>Beginner</p>
+                        </div>
+                        <div class="content">
+                            <p>command line</p>
+                            <p>Beginner</p>
+                        </div>
+                        <div class="content">
+                            <p>figma</p>
+                            <p>Beginner</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="body__left--languages">
+                    <h3>Languages</h3>
+                    <div class="content">
+                        <p>Slovak</p>
+                        <p>Native</p>
+                    </div>
+                    <div class="content">
+                        <p>Hungarian</p>
+                        <p>Advanced</p>
+                    </div>
+                    <div class="content">
+                        <p>English</p>
+                        <p>B2 Certified</p>
+                    </div>
+                    <div class="content">
+                        <p>Czech</p>
+                        <p>Advanced</p>
+                    </div>
+                    <div class="content">
+                        <p>Spanish</p>
+                        <p>Beginner</p>
+                    </div>
+                </div>
+                <div class="body__left--hobbies">
+                    <h3>Hobbies</h3>
+                    <div class="content">
+                        <a href="https://www.instagram.com/kikaortutova/">
+                            <i class="material-symbols-outlined md-36">
+                                photo_camera
+                            </i>
+                        </a>
+                        <i class="material-symbols-outlined md-36">
+                            videocam
+                        </i>
+                        <a href="https://github.com/ort01">
+                            <i class="material-symbols-outlined md-36">
+                                keyboard
+                            </i>
+                        </a>
+                        <i class="material-symbols-outlined md-36">
+                            pets
+                        </i>
+                        <br />
+                        <a href="https://pbs.twimg.com/media/FKbiaifWUAEvTg0?format=jpg&name=small">
+                            <i class="material-symbols-outlined md-36">
+                                sports_soccer
+                            </i>
+                        </a>
+                        <a href="https://i.pinimg.com/236x/06/f4/75/06f475b5b3be83d6f916abc4e53a2e1a.jpg">
+                            <i class="material-symbols-outlined md-36">
+                                cooking
+                            </i>
+                        </a>
+                        <a href="https://www.instagram.com/whenyouarequitebored/">
+                            <i class="material-symbols-outlined md-36">
+                                palette
+                            </i>
+                        </a>
+                        <i class="material-symbols-outlined md-36">
+                            flight_takeoff
+                        </i>
+                    </div>
+                </div>
+            </div>
+            <div class="body__right">
+                <div class="body__right--about-me">
+                    <h3>About Me</h3>
+                    <p>Junior <span class="highlight">Front-End</span> / Full-Stack Web Developer with passion for UI & UX,
+                        driven to
+                        learn new things. I enjoy working in an open team environment. I like to think I am
+                        very versatile, approachable and reliable perfectionist. Eager to apply and futher develop
+                        my skills.<br><br> I spend my spare time honing my skills in frontend development or for other
+                        creative activities, I tend to retreat to video editing, photograpy, music and painting.</p>
+                </div>
+                <div class="body__right--experience">
+                    <h3>Programming experience</h3>
+                    <p>In a hobbyist capacity, I have actively engaged with these technologies to develop and
+                        enhance personal projects. My experience working with <span class="highlight">Vue.js</span>,
+                        employing
+                        <span class="highlight">ES6+</span> syntax, both through
+                        the <span class="highlight">Options API</span>
+                        and the <span class="highlight">Composition API</span>, has allowed me to build dynamic and
+                        interactive
+                        web applications. I
+                        have utilized <span class="highlight">SCSS</span> to provide a visually appealing
+                        <span class="highlight">Responsive</span> and <span class="highlight">Cross-Browser Compatible
+                            UX</span>. Additionally,
+                        I
+                        have integrated <span class="highlight">REST APIs</span> and <span class="highlight">GraphQL</span>
+                        into Vue.js projects
+                        to efficiently retrieve and
+                        manage data. By leveraging <span class="highlight">Vite</span>, I have optimized the development
+                        workflow.
+                    </p>
+                </div>
+                <div class="body__right--work">
+                    <h3>Work Experience</h3>
+                    <div class="content">
+                        <p>Nemocnica AGEL Komárno <br /><span style="font-weight:100">Radiology technologist</span></p>
+                        <p>2018 - currently</p>
+                    </div>
+                </div>
+                <div class="body__right--education">
+                    <h3>Education</h3>
+                    <div class="content">
+                        <p>Slovenská zdravotnícka univerzita <br /> v Bratislave <span style="font-weight:100">- Bachelor
+                                degree</span></p>
+                        <p>2015 - 2018</p>
+                    </div>
+                    <div class="content">
+                        <p>Gymnázium Ľudovíta Jaroslava Šuleka <br>v Komárne
+                            <span style="font-weight:100">- Graduation</span>
+                        </p>
+                        <p>2011 - 2015</p>
+                    </div>
+                    <div class="content">
+                        <p>Základná umelecká škola v Komárne</p>
+                        <p>2002 - 2016</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </page>
+</template>
+
+
+
+<script setup>
+</script>
+
+
+
+<style lang="scss" scoped>
+.header {
+    &__background {
+        padding: 45% 75%;
+        background: $color-beige-light;
+        border-radius: 50%;
+        position: relative;
+        margin-top: -55%;
+        margin-left: -15%;
+        overflow: hidden;
+    }
+
+    &__content {
+        width: 100%;
+        position: absolute;
+        bottom: 0%;
+        right: 14%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        // gap: 40px;
+
+        &--left {
+            width: 25%;
+            margin-right: 10px;
+
+            & img {
+                width: 90%;
+                margin-bottom: 15px;
+            }
+        }
+
+        &--right {
+            margin-top: 20px;
+
+            & h1 {
+                text-transform: uppercase;
+                letter-spacing: 4px;
+            }
+
+            & h4 {
+                text-transform: uppercase;
+                letter-spacing: 2px;
+                margin-top: 10px;
+                font-weight: 200;
+            }
+
+            & hr {
+                margin-top: 10px;
+                width: 50%;
+                border: none;
+                border-bottom: 1px solid $color-beige-dark;
+            }
+
+            & a {
+                color: rgb($color-soft-black-light, .8);
+                margin-top: 20px;
+                width: fit-content;
+                display: flex;
+                align-items: center;
+                padding: 3px 8px;
+                gap: 10px;
+                font-size: 10px;
+                letter-spacing: 2px;
+                text-transform: uppercase;
+                border: solid 1px rgb($color-beige-dark, 1);
+                border-radius: 20px;
+            }
+
+            & a:nth-child(2) {
+                margin-top: 10px;
+            }
+        }
+    }
+}
+
+.body {
+    margin-top: 10px;
+    display: flex;
+    width: 100%;
+    height: 75%;
+
+    &__left {
+        width: 40%;
+        height: 95%;
+        padding: 0 30px;
+        border-right: 1px solid $color-beige-dark;
+
+        &--contact {
+            margin-top: -50px;
+
+            & .content {
+                display: flex;
+                align-items: center;
+                padding: 10px 0;
+                border-bottom: 1px solid rgb($color-beige-dark, 1);
+
+                i {
+                    padding: 0 5px;
+                    border-right: 1px solid rgb($color-beige-dark, 1);
+                    margin-right: 10px;
+                }
+            }
+        }
+
+        &--skills {
+
+            margin-top: 35px;
+
+            & .content {
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                text-transform: uppercase;
+                font-size: 10px;
+                letter-spacing: 1px;
+                padding-right: 40px;
+                line-height: 19px;
+            }
+        }
+
+        &--languages {
+            margin-top: 35px;
+
+            & .content {
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                text-transform: uppercase;
+                font-size: 10px;
+                letter-spacing: 1px;
+                padding-right: 40px;
+                line-height: 19px;
+            }
+        }
+
+        &--hobbies {
+            margin-top: 35px;
+
+            & i {
+                padding: 5px 10px;
+            }
+        }
+    }
+
+    &__right {
+        margin-top: 10px;
+        width: 60%;
+        padding: 0 40px;
+
+        &--about-me {
+
+            & p {
+                font-size: 13px;
+                text-align: justify;
+                line-height: 18px;
+                font-weight: 300;
+            }
+        }
+
+        &--experience {
+            margin-top: 40px;
+
+            & p {
+                font-size: 13px;
+                text-align: justify;
+                line-height: 18px;
+                font-weight: 300;
+            }
+        }
+
+        &--work {
+            margin-top: 40px;
+
+            & .content {
+                font-weight: 300;
+                font-size: 13px;
+                width: 90%;
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+            }
+        }
+
+        &--education {
+            margin-top: 40px;
+
+            & .content {
+                font-weight: 300;
+                margin-bottom: 15px;
+                font-size: 13px;
+                width: 90%;
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+            }
+        }
+
+    }
+}
+
+
+i {
+    color: $color-soft-black-light;
+    font-weight: 100;
+}
+
+
+h3 {
+    text-transform: uppercase;
+    letter-spacing: 3px;
+    font-weight: 400;
+    margin-bottom: 15px;
+}
+
+.highlight {
+    font-weight: 500;
+}
+
+.skills-separation {
+    padding-bottom: 5px;
+    margin-bottom: 5px;
+    border-bottom: 1px solid $color-beige-dark;
+}
+
+
+// }
+</style>
